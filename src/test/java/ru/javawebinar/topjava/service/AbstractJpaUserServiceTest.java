17ef5d0e0748e73f72e55c9019da9422e583c05f
Joanna Pakosh <morlineday@ukr.net>
2018-08-09 15:47:23 +0300

    HW06_Optional.2.2. adding new role to ADMIN

---
+++ src/test/java/ru/javawebinar/topjava/service/AbstractJpaUserServiceTest.java
@@ -0,0 +1,21 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.javawebinar.topjava.repository.JpaUtil;
+
+/**
+ * Created by Joanna Pakosh on Авг., 2018
+ */
+public class AbstractJpaUserServiceTest extends AbstractUserServiceTest {
+
+    @Autowired
+    protected JpaUtil jpaUtil;
+
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        jpaUtil.clear2ndLevelHibernateCache();
+    }
+}
