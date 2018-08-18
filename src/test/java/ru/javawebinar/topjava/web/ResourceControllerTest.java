8683aecafffa22887245b7bf6ea78588d1e454ca
Joanna Pakosh <morlineday@ukr.net>
2018-08-18 15:58:37 +0300

    HW07_1.2. ResourceControllerTest for style.css

---
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -0,0 +1,21 @@
+package ru.javawebinar.topjava.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by Joanna Pakosh on Авг., 2018
+ */
+public class ResourceControllerTest extends AbstractControllerTest {
+
+    @Test
+    void checkStatusAndContentType() throws Exception {
+        mockMvc.perform(get("/resources/css/style.css").contentType(MediaType.valueOf("text/css")))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}
