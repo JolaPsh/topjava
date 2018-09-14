4615621db63658640059f84dfd14a0b6502b7a73
Joanna Pakosh <morlineday@ukr.net>
2018-09-14 15:02:35 +0300

    app_ru.properties, View -> changed

--- src/main/java/ru/javawebinar/topjava/View.java
+++ src/main/java/ru/javawebinar/topjava/View.java
@@ -3,6 +3,6 @@ package ru.javawebinar.topjava;
 import javax.validation.groups.Default;
 
 public class View {
-    public interface Persist extends Default {
+    public interface Web extends Default {
     }
 }
