42406a4dc62f27d4f8701426bb5773a744eebf66
Joanna Pakosh <morlineday@ukr.net>
2018-09-16 23:09:25 +0300

    View.Persist-> added

--- src/main/java/ru/javawebinar/topjava/View.java
+++ src/main/java/ru/javawebinar/topjava/View.java
@@ -5,4 +5,8 @@ import javax.validation.groups.Default;
 public class View {
     public interface Web extends Default {
     }
+
+    // Validate only when DB save/update
+    public interface Persist extends Default {
+    }
 }
